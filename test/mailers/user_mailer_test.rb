26793f8e192fe1e14f8c1b1c021d4b4ea0b2c346
Jonathan Lui <lui.jonathanc@gmail.com>
2015-02-21 12:29:22 -0800

    Reset and activation now only make one database update instead of two calls.

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -8,7 +8,7 @@ class UserMailerTest < ActionMailer::TestCase
 
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@goaround.com"], mail.from
     assert_match user.first_name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
@@ -20,7 +20,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@goaround.com"], mail.from
     assert_match user.reset_token, mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
