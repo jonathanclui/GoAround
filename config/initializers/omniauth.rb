a1397caba23fd0e442f7fff3d4a41af22252f323
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-03 15:00:02 -0700

    Signing in now directs directly to the home page URL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :uber, ENV['UBER_CLIENT_ID'], ENV['UBER_CLIENT_SECRET'], :scope => 'profile, history'
 end
