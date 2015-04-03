65b6c52e779f9e68a05c248f92be37c699614f67
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-03 11:47:36 -0700

    Fixed heroku error by first checking if a file exists before trying to read

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-    ENV[key] = value.to_s unless value.kind_of? Hash
-end
+if File.exists?(File.expand_path('../application.yml', __FILE))
+    config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+    config.merge! config.fetch(Rails.env, {})
+    config.each do |key, value|
+        ENV[key] = value.to_s unless value.kind_of? Hash
+    end
+ends
 
 module GoAround
   class Application < Rails::Application
