dc108a1e854c989383e57956828eb9aea8fb2f44
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-05 13:25:35 -0700

    Changed the listeners to be generated dynamically for each type so the listener will exist if a new uberType is released

--- app/assets/javascripts/eventListeners.js
+++ app/assets/javascripts/eventListeners.js
@@ -45,33 +45,3 @@ $(document).on('mouseover', "#walkingResults", function() {
 	var mode = "WALKING";
 	populateResults(mode);
 });
-
-// Listeners to change the color of the active item in uber panel
-$(document).on("mouseover", "#uberX", function() {
-	setActiveUberClass(this);
-	drawUberInfo("uberX");
-});
-$(document).on("mouseover", "#uberXL", function() {
-	setActiveUberClass(this);
-	drawUberInfo("uberXL");
-});
-$(document).on("mouseover", "#UberBLACK", function() {
-	setActiveUberClass(this);
-	drawUberInfo("UberBLACK");
-});
-$(document).on("mouseover", "#UberSUV", function() {
-	setActiveUberClass(this);
-	drawUberInfo("UberSUV");
-});
-$(document).on("mouseover", "#uberTAXI", function() {
-	setActiveUberClass(this);
-	drawUberInfo("uberTAXI");
-});
-$(document).on("mouseover", "#uberPLUS", function() {
-	setActiveUberClass(this);
-	drawUberInfo("uberPLUS");
-});
-$(document).on("mouseover", "#UberLUX", function() {
-	setActiveUberClass(this);
-	drawUberInfo("UberLUX");
-});
