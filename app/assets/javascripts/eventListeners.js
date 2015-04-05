46758023d8f82336e908d8f290cd32b1b520ba3b
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-04 18:33:52 -0700

    Map now appears without the need to manually refresh the app. Listeners are now on the document level and persist betwen pages

--- app/assets/javascripts/eventListeners.js
+++ app/assets/javascripts/eventListeners.js
@@ -1,53 +1,49 @@
-// Add listeners after the page loads for the map
-$(document).ready(function() { 
-	setActiveClass = function(newActive) {
-		$("li.active").removeClass("active");
-		$(newActive).addClass("active");
-	};
+setActiveClass = function(newActive) {
+	$("li.active").removeClass("active");
+	$(newActive).addClass("active");
+};
 
-	setActiveUberClass = function(newActive) {
-		$("li.uberActive").removeClass("uberActive");
-		$(newActive).addClass("uberActive");
-	};
+setActiveUberClass = function(newActive) {
+	$("li.uberActive").removeClass("uberActive");
+	$(newActive).addClass("uberActive");
+};
 
-	// Add an event listener to submit query when enter key is pressed and focus is in either start or end box
-	window.addEventListener("keypress", function(e) {
-		if (e.which == 13 && (($(':focus').activeElement == $("#start_input").activeElement) || ($(':focus').activeElement == $("#end_input").activeElement))) {
-			populateResultsFromUser();
-		}
-	});
+window.addEventListener("keypress", function(e) {
+	if (e.which == 13 && (($(':focus').activeElement == $("#start_input").activeElement) || ($(':focus').activeElement == $("#end_input").activeElement))) {
+		populateResultsFromUser();
+	}
+});
 
-	// Listeners to change the color of the active item
-	$("#drivingResults").mouseover(function() {
-		setActiveClass(this);
+// Listeners to change the color of the active item
+$(document).on('mouseover', "#drivingResults", function() {
+	setActiveClass(this);
 
-		var mode = "DRIVING";
-		populateResults(mode);
-	});
-	$("#publicTransitResults").mouseover(function() {
-		setActiveClass(this);
+	var mode = "DRIVING";
+	populateResults(mode);
+});
+$(document).on('mouseover', "#publicTransitResults", function() {
+	setActiveClass(this);
 
-		var mode = "TRANSIT";
-		populateResults(mode);
-	});
-	$("#uberResults").mouseover(function() {
-		setActiveClass(this);
+	var mode = "TRANSIT";
+	populateResults(mode);
+});
+$(document).on('mouseover', "#uberResults", function() {
+	setActiveClass(this);
 
-		var mode = "UBER";
-		populateResults(mode);
-	});
-	$("#bikingResults").mouseover(function() {
-		setActiveClass(this);
+	var mode = "UBER";
+	populateResults(mode);
+});
+$(document).on('mouseover', "#bikingResults", function() {
+	setActiveClass(this);
 
-		var mode = "BICYCLING";
-		populateResults(mode);
-	});
-	$("#walkingResults").mouseover(function() {
-		setActiveClass(this);
+	var mode = "BICYCLING";
+	populateResults(mode);
+});
+$(document).on('mouseover', "#walkingResults", function() {
+	setActiveClass(this);
 
-		var mode = "WALKING";
-		populateResults(mode);
-	});
+	var mode = "WALKING";
+	populateResults(mode);
 });
 
 // Listeners to change the color of the active item in uber panel
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -168,3 +168,4 @@ function getGeocodeLongitude(result) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'page:load', initialize);
